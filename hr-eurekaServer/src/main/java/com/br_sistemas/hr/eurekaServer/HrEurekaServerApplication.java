644cb3738c8b3cdd5e45ff2f0a3e2cb2217e99e2
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2024-09-14 08:21:33 -0300

    Project hr-eurekaServer created

---
+++ hr-eurekaServer/src/main/java/com/br_sistemas/hr/eurekaServer/HrEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.br_sistemas.hr.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+		
+	}
+
+}
---
+++ hr-eurekaServer/src/test/java/com/br_sistemas/hr/eurekaServer/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br_sistemas.hr.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
