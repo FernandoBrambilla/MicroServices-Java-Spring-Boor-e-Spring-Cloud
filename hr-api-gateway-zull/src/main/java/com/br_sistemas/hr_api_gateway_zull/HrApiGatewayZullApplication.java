914dbbb6b1d4558aed741d5a9b63a872999b2c5f
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2024-10-04 13:04:59 -0300

    Project hr-api-gateway-zull created

---
+++ hr-api-gateway-zull/src/main/java/com/br_sistemas/hr_api_gateway_zull/HrApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package com.br_sistemas.hr_api_gateway_zull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZullApplication.class, args);
+	}
+
+}
