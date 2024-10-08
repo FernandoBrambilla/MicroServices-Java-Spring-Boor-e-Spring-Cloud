41249b1929b50d04fce3a603780cfa5991acf1ee
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2024-10-08 14:52:04 -0300

    Project oauth created

---
+++ hr-oauth/src/test/java/com/br_sistemas/hr_oauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br_sistemas.hr_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
