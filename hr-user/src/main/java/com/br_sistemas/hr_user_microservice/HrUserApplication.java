af295450563f5e12637057a480d08e7e3430c24d
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2024-10-08 13:21:53 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/br_sistemas/hr_user_microservice/HrUserApplication.java
@@ -0,0 +1,13 @@
+package com.br_sistemas.hr_user_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
---
+++ hr-user/src/test/java/com/br_sistemas/hr_user_microservice/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br_sistemas.hr_user_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
