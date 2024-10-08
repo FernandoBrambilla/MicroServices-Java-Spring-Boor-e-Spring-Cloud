02299a272199f1c53d3bb2e0e024e84d78846929
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2024-10-08 13:28:06 -0300

    Configure hr-user project

--- hr-user/src/main/java/com/br_sistemas/hr_user_microservice/HrUserApplication.java
+++ hr-user/src/main/java/com/br_sistemas/hr_user_microservice/HrUserApplication.java
@@ -2,7 +2,9 @@ package com.br_sistemas.hr_user_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
